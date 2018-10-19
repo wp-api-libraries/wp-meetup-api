62076e14657264fd14f0c0de5d321a1a4e49fb4d
Brandon Hubbard <bhubbard@users.noreply.github.com>
2018-10-19 10:49:44 -0700

    Update wp-meetup-api.php

--- wp-meetup-api.php
+++ wp-meetup-api.php
@@ -59,7 +59,16 @@ class MeetupAPI extends WpMeetupBase {
     $this->api_key = $api_key;
     $this->is_debug     = $is_debug;
   }
-
+  
+  /**
+   * build_request function.
+   * 
+   * @access public
+   * @param string $route (default: '')
+   * @param array $args (default: array())
+   * @param string $method (default: 'GET')
+   * @return void
+   */
   public function build_request( $route = '', $args = array(), $method = 'GET' ){
     $args['key'] = $this->api_key;
 
@@ -79,10 +88,93 @@ class MeetupAPI extends WpMeetupBase {
     //   'Content-Type'       => 'application/json'
     // );
   }
-
+  
+  /**
+   * run function.
+   * 
+   * @access private
+   * @param mixed $route
+   * @param array $args (default: array())
+   * @param string $method (default: 'GET')
+   * @return void
+   */
   private function run( $route, $args = array(), $method = 'GET' ){
     return $this->build_request( $route, $args, $method )->fetch();
   }
+  
+  // ABUSE.
+  
+  // BATCH.
+  
+  // BOARDS.
+  
+  /**
+   * get_group_boards function.
+   * 
+   * @access public
+   * @param mixed $url_name
+   * @param mixed $fields (default: null)
+   * @return void
+   */
+  public function get_group_boards( $url_name, $fields = null ) {
+	  return $this->run( $url_name . '/boards', array( 'fields' => $fields ) );
+  }
+  
+  /**
+   * get_group_board_discussions function.
+   * 
+   * @access public
+   * @param mixed $url_name
+   * @param mixed $board_id
+   * @param mixed $fields (default: null)
+   * @return void
+   */
+  public function get_group_board_discussions( $url_name, $board_id, $fields = null ) {
+	  return $this->run( $url_name . '/boards/' . $board_id . '/discussions', array( 'fields' => $fields ) );
+  }
+  
+  /**
+   * get_group_board_discussions_posts function.
+   * 
+   * @access public
+   * @param mixed $url_name
+   * @param mixed $board_id
+   * @param mixed $discussion_id
+   * @param mixed $fields (default: null)
+   * @return void
+   */
+  public function get_group_board_discussions_posts( $url_name, $board_id, $discussion_id, $fields = null ) {
+	  return $this->run( $url_name . '/boards/' . $board_id . '/discussions/' . $discussion_id, array( 'fields' => $fields ) );
+  }
+  
+  // CATEGORIES.
+  
+  /**
+   * get_categories function.
+   * 
+   * @access public
+   * @param mixed $fields (default: null)
+   * @return void
+   */
+  public function get_categories( $fields = null ) {
+	   return $this->run( '2/categories', array( 'fields' => $fields ) );
+  }
+  
+  // CITIES.
+  
+  /**
+   * get_cities function.
+   * 
+   * @access public
+   * @param mixed $fields (default: null)
+   * @return void
+   */
+  public function get_cities( $fields = null ) {
+	  return $this->run( '2/cities', array( 'fields' => $fields ) );
+  }
+  
+  
+  // DASHBOARD.
 
   /**
    * Get a dashboard of aggregated Meetup information for the authorized member
@@ -92,4 +184,233 @@ class MeetupAPI extends WpMeetupBase {
   public function get_dashboard( $fields = null ){
     return $this->run( 'dashboard', array( 'fields' => $fields ) );
   }
+  
+  // COMMENTS.
+  
+  /**
+   * get_event_comments function.
+   * 
+   * @access public
+   * @param mixed $url_name
+   * @param mixed $event_id
+   * @param mixed $fields (default: null)
+   * @return void
+   */
+  public function get_event_comments( $url_name, $event_id, $fields = null ) {
+	 return $this->run( $url_name . '/events/' . $event_id . '/comments', array( 'fields' => $fields ) );
+  }
+  
+  /**
+   * get_event_comment_likes function.
+   * 
+   * @access public
+   * @param mixed $url_name
+   * @param mixed $event_id
+   * @param mixed $comment_id
+   * @param mixed $fields (default: null)
+   * @return void
+   */
+  public function get_event_comment_likes( $url_name, $event_id, $comment_id, $fields = null ) {
+	  return $this->run( $url_name . '/events/' . $event_id . '/comments/' . $comment_id . '/likes', array( 'fields' => $fields ) );
+  }
+  
+  // EVENTS.
+  
+  /**
+   * get_events function.
+   * 
+   * @access public
+   * @param mixed $url_name
+   * @param mixed $fields (default: null)
+   * @return void
+   */
+  public function get_events( $url_name, $fields = null ) {
+	  return $this->run( $url_name . '/events/', array( 'fields' => $fields ) );
+  }
+  
+  /**
+   * get_event function.
+   * 
+   * @access public
+   * @param mixed $url_name
+   * @param mixed $event_id
+   * @param mixed $fields (default: null)
+   * @return void
+   */
+  public function get_event( $url_name, $event_id, $fields = null ) {
+	   return $this->run( $url_name . '/events/' . $event_id, array( 'fields' => $fields ) );
+  }
+  
+  /**
+   * get_event_attendance function.
+   * 
+   * @access public
+   * @param mixed $url_name
+   * @param mixed $event_id
+   * @param mixed $fields (default: null)
+   * @return void
+   */
+  public function get_event_attendance( $url_name, $event_id, $fields = null ) {
+	  return $this->run( $url_name . '/events/' . $event_id . '/attedance', array( 'fields' => $fields ) );
+  }
+  
+  /**
+   * find_upcoming_events function.
+   * 
+   * @access public
+   * @param mixed $fields (default: null)
+   * @return void
+   */
+  public function find_upcoming_events( $fields = null ) {
+	  return $this->run( 'find/upcoming_events/', array( 'fields' => $fields ) );
+  }
+  
+  /**
+   * get_self_calendar function.
+   * 
+   * @access public
+   * @param mixed $fields (default: null)
+   * @return void
+   */
+  public function get_self_calendar( $fields = null ) {
+	   return $this->run( 'self/calendar', array( 'fields' => $fields ) );
+  }
+  
+  /**
+   * get_self_events function.
+   * 
+   * @access public
+   * @param mixed $fields (default: null)
+   * @return void
+   */
+  public function get_self_events( $fields = null ) {
+	   return $this->run( 'self/events', array( 'fields' => $fields ) );
+  }
+  
+  
+  // FEEDS.
+  
+  public function get_activity( $fields = null ) {
+	  return $this->run( 'activity', array( 'fields' => $fields ) );
+  }
+  
+  // GEO.
+  
+  public function find_locations() {
+	  
+  }
+  
+  // GROUPS.
+   
+  /**
+   * get_group function.
+   * 
+   * @access public
+   * @param mixed $url_name
+   * @param mixed $fields (default: null)
+   * @return void
+   */
+  public function get_group( $url_name, $fields = null ) {
+	  return $this->run( $url_name, array( 'fields' => $fields ) );
+  }
+  
+  public function create_group() {
+	  // TODO:
+  }
+  
+  public function get_similar_groups( $url_name, $fields = null ) {
+	  
+  }
+  
+  public function add_group_topics() {
+	  
+  }
+  
+  public function delete_group_topics() {
+	  
+  }
+  
+  public function find_groups() {
+	  
+  }
+  
+  public function get_recommended_groups() {
+	  
+  }
+  
+  public function add_recommend_groups_ignores() {
+	  
+  }
+  
+  /**
+   * get_self_groups function.
+   * 
+   * @access public
+   * @param mixed $fields (default: null)
+   * @return void
+   */
+  public function get_self_groups( $fields = null ) {
+	  return $this->run( 'self/groups', array( 'fields' => $fields ) );
+  }
+  
+  // HOSTS.
+  
+  // MEMBERS.
+  
+  // META.
+  
+  // NOTIFICATIONS.
+  
+  // PHOTOS.
+  
+  // PREFERENCES.
+  
+  // PRO.
+  
+  // PROFILES.
+  
+  
+  
+  // RSVPS.
+  
+  public function get_group_event_rsvps() {
+	  
+  }
+  
+  public function add_group_event_rsvps() {
+	  
+  }
+  
+  // TOPICS.
+  
+  public function find_topic_categories() {
+	  
+  }
+  
+  public function find_topics() {
+	  
+  }
+  
+  public function get_recommended_group_topics() {
+	  
+  }
+  
+  // VENUES.
+  
+  public function get_group_venues() {
+	  
+  }
+  
+  public function add_group_venue() {
+	  
+  }
+  
+  public function find_venues() {
+	  
+  }
+  
+  public function get_recommended_venues() {
+	  
+  }
+  
 }
